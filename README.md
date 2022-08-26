5f18dc2ae874630b2aa770865df2e9d36e5e0df9
vegetax <56632283@qq.com>
2022-08-26 13:55:04 +0800

    1

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ npx hardhat run scripts/deploy.js
 ### .yarnrc.yml中的nodeLinker: node-modules 控制开关  PnP模式，有是关闭，如需打开则删除这行就行
 ### yarn hardhat console --network rinkeby  可以直接在console里与rinkeby交互
 await ethers.provider.getBlockNumber()
+### super示例合约 kovan地址 0xB789B26e12BCBbb334367c66807F0a63aB59cA0F
+###  yarn hardhat run scripts/SFdeploy.js --network kovan 可发起一个流
--- scripts/SFdeploy.js
+++ scripts/SFdeploy.js
@@ -1,7 +1,33 @@
-import { Framework } from "@superfluid-finance/sdk-core";
-import { hardhat, ethers } from "hardhat";
+const { Framework } = require("@superfluid-finance/sdk-core")
+const { ethers } = require("hardhat")
+require("dotenv").config()
 
-const injectedHardhatEthersSf = await Framework.create({
-  chainId: 31337,
-  provider: hardhat.ethers,
-});
+async function main() {
+  const ContractAddress = "0xB789B26e12BCBbb334367c66807F0a63aB59cA0F"
+  const sf = await Framework.create({
+    chainId: 42, //your chainId here
+    provider: ethers.provider,
+  })
+
+  const signer = sf.createSigner({
+    privateKey: process.env.PRIVATE_KEY,
+    provider: ethers.provider,
+  })
+
+  const DAIx = await sf.loadSuperToken("fDAIx")
+
+  const CreatFl = sf.cfaV1.createFlow({
+    receiver: ContractAddress,
+    superToken: DAIx.address,
+    flowRate: "100000000000",
+  })
+
+  const txn = await CreatFl.exec(signer)
+  const receipt = await txn.wait()
+  console.log(receipt)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
